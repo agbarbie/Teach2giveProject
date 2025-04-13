f8cb2eb8eb523e577d65d645c97edb8b9ab285d3
agbarbie <barbarawangui2002@gmail.com>
2025-04-14 01:26:21 +0300

    updates

--- .github/actions/main.js
+++ .github/actions/main.js
@@ -1,23 +1,24 @@
-const core = require('@actions/core');//input and output
-const exec = require('@actions/exec');//uploading files to s3
+const core = require('@actions/core'); // Input and output
+const exec = require('@actions/exec'); // Uploading files to S3
 
+async function run() {
+  try {
+    // Getting inputs
+    const bucket = core.getInput('bucket', { required: true }); // Bucket name
+    const bucketRegion = core.getInput('bucket-region', { required: true }); // Bucket region
+    const distFolder = core.getInput('dist-folder', { required: true }); // Dist folder name
 
-function run() {
-//getting inputs
-    const bucket = core.getInput('bucket',{required:true});//bucket name
-    const bucketRegion = core.getInput('bucket-region',{required:true});//bucket region
-    const distFolder = core.getInput('dist-folder',{required:true});//dist folder name
+    // Uploading files to S3
+    const s3Url = `s3://${bucket}`; // Correctly interpolated S3 URL
+    await exec.exec('aws', ['s3', 'sync', distFolder, s3Url, '--region', bucketRegion]);
 
-
-//uploading files to s3
-const s3Url = 's3://${bucket}';//s3 url
-exec.exec('aws s3 sync ${distFolder} ${s3Url} --region ${bucketRegion}')
-// 'aws s3 sync ${distFolder} ${s3Url} --region ${bucketRegion}'//aws s3 sync command
-
-
-//getting website url
-const websiteUrl = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`//website url
-// http://skillmatchesai.s3-website.eu-north-1.amazonaws.com
-core.setOutput('website-url', websiteUrl)//setting output
+    // Getting website URL
+    const websiteUrl = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`; // Website URL
+    core.setOutput('website-url', websiteUrl); // Setting output
+  } catch (error) {
+    // http://skillmatchesai.s3-website.eu-north-1.amazonaws.com
+    core.setFailed(error.message);
+  }
 }
-run()
+
+run();
